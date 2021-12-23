69f0760e4a4753ba6261850be916100077c445a0
김소연 <soyeon.dev@gmail.com>
2021-12-23 21:51:58 +0900

    router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import List from "./pages/list";
+import Detail from './pages/detail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header>This is header</header>
+      <Routes>      
+        <Route path="/list" element={<List/>} />
+        <Route path="/detail:detail_id" element={<Detail/>} />
+      </Routes>
     </div>
   );
 }
